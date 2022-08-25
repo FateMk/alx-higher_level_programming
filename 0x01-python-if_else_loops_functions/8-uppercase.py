b61e99d2e6e48f054a0774becbb343119e6835f1
FateMk <93762003+FateMk@users.noreply.github.com>
2022-08-25 03:38:32 +0300

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     word = list(str)
     for i in range(len(word)):
         if (ord(word[i]) > 96 and ord(word[i]) < 123):
-            wprd[i] = chr(ord(word[i]) - 32)
+            word[i] = chr(ord(word[i]) - 32)
     print("{}".format(("").join(word)))
