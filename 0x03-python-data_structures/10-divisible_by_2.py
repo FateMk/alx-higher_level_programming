d66585668ea3eadd192ee0704650ccf1deac5308
FateMk <93762003+FateMk@users.noreply.github.com>
2022-08-30 02:38:41 +0300

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     if my_list is None:
         return
     multiples =[]
-    for i in range(len(my_list)):
+    for i in my_list:
         if i % 2 == 0:
             multiples.append(True)
        else:
