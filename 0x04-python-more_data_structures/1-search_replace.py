5c052b721fdad38bd9cac45b66eafa296129bafa
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-03 03:00:58 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in my_list:
-        new_list = my_list
-        if i == search:
-            replace = search
     return [replace if search == n else n for n in my_list]
-
