738ebcded658fb85d7eed82c9a4d14f049d9407b
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-15 04:10:20 +0300

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError:
-        return None
+    except:
+        result = None
     finally:
         print("{:d} / {:d} = {:d}".format(a, b, result))
         return result
