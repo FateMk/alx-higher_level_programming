9e9c42ab2f9cceac2f2728d7fe85c82a1b23f2fc
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-21 02:31:51 +0300

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defining class Rectangle"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
@@ -39,17 +39,22 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        
+
     def area(self):
-        return self.__height * self.__width
-    
+        """Return the area of the Rectangle."""
+        return (self.__width * self.__height)
+
     def perimeter(self):
+        """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return ((self.__width + slef.__height) * 2)
-    
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
     def __str__(self):
-       if self.__width == 0 or self.__height == 0:
+        """Return the printable representation of the Rectangle.
+        Represents the rectangle with the # character.
+        """
+        if self.__width == 0 or self.__height == 0:
             return ("")
 
         rect = []
