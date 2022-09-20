d0a46a713dc9b5aca0e6e8fd2427ad22d1661d4c
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-21 02:31:18 +0300

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -48,7 +48,7 @@ class Rectangle:
             return 0
         return ((self.__width + slef.__height) * 2)
     
-    def str(self):
+    def __str__(self):
        if self.__width == 0 or self.__height == 0:
             return ("")
 
