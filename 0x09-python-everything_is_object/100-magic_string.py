166a7f6d10f91b19a9c39d894f98044d10a86599
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-23 04:07:36 +0300

    Everything is object

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+count = 0
+def magic_string():
+    global count++
+    return ("BestSchool" * count)
+
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Defining class LockedClass"""
+
+
+class LockedClass:
+     __slots__ = ["first_name"
