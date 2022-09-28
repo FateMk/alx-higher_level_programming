2006928a0f2ae17cd6650d8cfb27c7e150463504
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 00:47:53 +0300

    To JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""" module that contains a function that Returns json
+representation of an object
+"""
+
+
 import json
 
 
 def to_json_string(my_obj):
+    """ Function that returns the JSON representation of an object
+    Args:
+        my_obj: object
+    Raises:
+        Exception: when the object can't be encoded
+    """
     return json.dumps(my_obj)
