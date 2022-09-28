47872530396abc685ef18fe6b1deaedeba03e5a6
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 01:09:08 +0300

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""Module contains a function that creates an 
+Object from a “JSON file"
+"""
 import json
 
 
 def load_from_json_file(filename):
+    """Function that creates an object from JSON file
+    Args:
+        filaname: JSON representation
+    Raises:
+        Exception: when the object can't be encoded
+    """
     with open(filename, encoding="utf-8") as f:
         return json.load(f)
