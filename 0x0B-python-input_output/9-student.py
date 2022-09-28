01367a14ffbfa9a9d088f9c5999b0b93ca0ff28b
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 01:20:46 +0300

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,16 +3,14 @@
 
 
 class Student:
+    """class that creates a student instance"""
+    
     def __init__(self, first_name, last_name, age):
         """initializing new instance of student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+        
     def to_json(self):
-        """returns dict attributes of student"""
-        try:
-            obj_dict = self._dict_
-            return obj_dict
-        except:
-            return {}
-
+        """ Method that returns directory description """
+        return self.__dict__.copy()
