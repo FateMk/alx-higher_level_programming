9378c6e7edae54c67fc183f1791f5837ede36f2e
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 01:24:48 +0300

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,11 +3,14 @@
 
 
 class Student:
+     """ Class to create student instances """
+        
     def __init__(self, first_name, last_name, age):
         """initializing new instance of student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+        
     def to_json(self, attrs=None):
         """returns dict attributes of student"""
         if attrs is None:
