5d280e06080cccf4690590375027f009b8353c74
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 01:02:44 +0300

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,10 +8,10 @@ import json
 def save_to_json_file(my_obj, filename):
     """Functin writes an object to text file in JSON
     Args:
-        my_obj: JSON representation
-        filename: text file
+        my_obj: object
+        filename: textfile name
     Raises:
         Exception: when the string can't be decoded
     """
-    with open(filename, mode='w', encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         f.write(json.dumps(obj))
