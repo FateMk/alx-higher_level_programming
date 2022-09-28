fdbfa09095ecdc2b0878fa35e6f0ec4978ee6608
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 00:31:16 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with  open(filename, mode='w', encoding="utf-8") as f:
+    with  open(filename, mode='r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end='')
