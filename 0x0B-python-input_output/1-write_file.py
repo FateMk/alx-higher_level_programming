6ad9028a003fc38eac77cdf1973d3fbe815fac74
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 00:36:25 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+""" Module that contains a function that writes to a text file
+"""
+
 def write_file(filename="", text=""):
+    """ Function that writes to a text file
+    Args:
+        filename: filename
+        text: text to write
+    Raises
+        Exception: when the file can be opened
+    """
+    
     with open(filename, encoding="utf-8") as f:
-        f.write(text)
-    return len(text)    
+        return f.write(text)
