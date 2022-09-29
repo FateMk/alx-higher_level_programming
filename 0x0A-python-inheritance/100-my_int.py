d4a7f1a05585a440f47082ad397aa2f6d94e7d7a
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 04:38:40 +0300

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-class MyInt(int):
-    def __equal__(self, value):
-        return self - value != 0
+"""Contains the class MyInt"""
 
-    def __ne__(self, value):
-        return self - value == 0
+
+class MyInt(int):
+    """rebel version of an integer, perfect for opposite day!"""
+    def __new__(cls, *args, **kwargs):
+        """create a new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        """what was != is now =="""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """what was == is now !="""
+        return int(self) == other
