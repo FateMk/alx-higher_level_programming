188a33e86aef6ff5b5bd5ca5b0b054e65e23f864
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 03:53:21 +0300

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,4 +6,4 @@ This module contains the function is_same_class
 
 def is_sane_class(obj, a_class):
     """returns True if the object is exactly a class otherwise False"""
-    return (obj.__class__ == a_class)
+    return (type(obj) == a_class)
