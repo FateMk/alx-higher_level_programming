aaae9a4e0456bf17a49bd81b8e72fdf1a655fac4
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 04:34:21 +0300

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/pyhton3
+"""contains class Square subclass of Rectangle"""
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """A representation of a square"""
     def __init__(self, size):
+        """initilization"""
         super().__init__(size, size)
         self.__size = size
 
-
     def __str__(self):
+        """prints square description"""
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
