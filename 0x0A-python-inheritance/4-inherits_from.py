06f4746a33e6ae09780605e59344b4ee89934a5d
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 03:56:49 +0300

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""module contains function inherits_from"""
+
+
 def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class that inherited 
+    otherwise False
+    """
     if (type(obj) != a_class):
         return issubclass(type(obj), a_class)
     return False
-
-
