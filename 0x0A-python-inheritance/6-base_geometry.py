eed854fd921be8dbb7947bda90db33d466f9b45a
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 04:04:42 +0300

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -4,7 +4,6 @@
 
 class BaseGeometry:
     """A class with public attributes"""
-    
     def area(self):
         """raises an exception when called"""
         raise Exception("area() is not implemented")
