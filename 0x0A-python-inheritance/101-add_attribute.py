f257afc91fec59caf561debc40a381a11923c606
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 04:40:45 +0300

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""
+method module
+"""
+
+
 def add_attribute(obj, a, v):
+        """add attribute to object
+    args:
+        obj: class object
+        objname: object name
+        value: value of attribute
+    return:
+        na
+    """
     res = getattr(obj, "__doc__", None)
     if res is None:
         setattr(obj, a, v)
