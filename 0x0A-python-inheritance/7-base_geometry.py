f586a798176454dc26d74a698b548fd5ecf06560
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 04:07:22 +0300

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""defining class BaseGoemetry"""
+
+
 class BaseGeometry:
+    """A class with public attributes"""
     def area(self):
+        """raises an exception ehen called"""
         raise Exception("area() is not implemeted")
+        
     def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
         if type(value) if not int:
             raise TypeError("<name> must be an integer")
         if (value[] <= 0):
