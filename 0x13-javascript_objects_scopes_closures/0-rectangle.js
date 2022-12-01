89ec4e1d7fb50c9481b41da957a0344df8d91f06
FateMk <93762003+FateMk@users.noreply.github.com>
2022-12-01 03:05:11 +0300

    JavaScript - Objects, Scopes and Closures

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* 0-rectangle.js */
+
+module.exports = {
+  Rectangle: Rectangle
+};
+
+function Rectangle () {
+}
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* 1-rectangle.js */
+
+module.exports = {
+  Rectangle: Rectangle
+};
+
+function Rectangle (w, h) {
+  this.width = w;
+  this.height = h;
+}
