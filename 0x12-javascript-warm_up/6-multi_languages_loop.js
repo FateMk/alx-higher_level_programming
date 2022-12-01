60985aed65888af88b5ff9ee6d7f44eaba37336d
FateMk <93762003+FateMk@users.noreply.github.com>
2022-12-01 03:36:10 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < strings.length; i++) {
-  console.log(strings[i]);
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const language of languages) {
+  console.log(language);
 }
