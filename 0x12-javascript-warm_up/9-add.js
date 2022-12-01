ad8cd31489384545a9b08b6de72b290cf748e57e
FateMk <93762003+FateMk@users.noreply.github.com>
2022-12-01 03:38:43 +0300

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-const first = parseInt(process.argv[2]);
-const second = parseInt(process.argv[3]);
+
 function add (a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return NaN;
-  }
   return a + b;
 }
-console.log(add(first, second));
+
+const args = process.argv;
+const num1 = parseInt(args[2], 10);
+const num2 = parseInt(args[3], 10);
+
+console.log(add(num1, num2));
