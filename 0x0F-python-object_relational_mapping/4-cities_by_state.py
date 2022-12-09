835d887943f53556371e09e546e7f8f2a72694f1
FateMk <93762003+FateMk@users.noreply.github.com>
2022-12-10 02:48:38 +0300

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-# List all cities from the db
-# Username, password, and database name given as user args
-# Can only use execute() once
-# Sort ascending order by cities.id
-import sys
+"""
+4-cities_by_state.py
+"""
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1],
+
+def init_db():
+    """initializes a db with MySQLdb"""
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         host='localhost',
-                         port=3306)
+                         db=sys.argv[3])
+    return db
+
+
+def print_cities(db):
+    """prints all cities from input DB"""
     cur = db.cursor()
-    cmd = """SELECT cities.id, cities.name, states.name
-         FROM states
-         INNER JOIN cities ON states.id = cities.state_id
-         ORDER BY cities.id ASC"""
-    cur.execute(cmd)
-    allCities = cur.fetchall()
-
-    for city in allCities:
-        print(city)
-
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                "INNER JOIN states ON states.id = cities.state_id "
+                "ORDER BY cities.id ASC")
+    for row in cur.fetchall():
+        print(row)
     cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    print_cities(init_db())
