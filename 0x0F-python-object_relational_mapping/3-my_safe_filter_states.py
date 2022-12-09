10954664357b831443acf193382e86ddf021bb58
FateMk <93762003+FateMk@users.noreply.github.com>
2022-12-10 02:47:39 +0300

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,40 +2,38 @@
 """
 3-my_safe_filter_states.py
 """
-import sys
 import MySQLdb
+import sys
 
 
 def init_db():
-    """initilizing a db with MySQLdb"""
-    db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],)
+    """initializes a db with MySQLdb"""
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
     return db
 
+
 def parse_input(s):
-    """paser input removing single quotes and semicolons"""
-    s = ''.join([i for i in s if i != "'" and i != ';')
+    """parses input removing single quotes and semicolons"""
+    s = ''.join([i for i in s if i != "'" and i != ';'])
     return s
 
 
-def print_state_searched(db):
-    """prints all states with a name stating with N"""
+def print_one_state(db):
+    """prints one state from input DB with SQL injection safeguard"""
     cur = db.cursor()
     name = parse_input(sys.argv[4])
-    cur.execute("SELECT * FROM states"
-                "WHERE name LIKE BINARY '%{}%'"
+    cur.execute("SELECT * FROM states "
+                "WHERE name LIKE BINARY '%{}%' "
                 "ORDER BY states.id ASC".format(name))
-    rows = cur.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
     cur.close()
     db.close()
 
 
 if __name__ == "__main__":
-    print_state_searched(int_db())
-
+    print_one_state(init_db())
