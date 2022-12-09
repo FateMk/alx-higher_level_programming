960912bcf8289601a378576379eb2cde2e046b67
FateMk <93762003+FateMk@users.noreply.github.com>
2022-12-10 02:52:55 +0300

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-# List all State objects from db 'hbtn_0e_6_usa'
-# Script should take 3 args: username, pw, and db name
-# Must use SQLAlchemy
+"""
+7-model_state_fetch_all.py
+"""
 import sys
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+
+def init_sess():
+    """initializes session and engine for sqlalchemy DB instance"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
+    return (engine, session)
 
-    for instance in session.query(State):
-        print("{:d}: {}".format(instance.id, instance.name))
+
+def print_states(db):
+    """prints all the states from session DB"""
+    session = db[1]
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, ': ', instance.name, sep='')
+    session.close()
+    db[0].dispose()
+
+if __name__ == '__main__':
+    print_states(init_sess())
