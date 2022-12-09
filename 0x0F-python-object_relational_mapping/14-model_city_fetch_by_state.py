ee1cedc862038aba6e8685f97f701efed8cc39ef
FateMk <93762003+FateMk@users.noreply.github.com>
2022-12-10 02:59:48 +0300

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
-# Print all City objs from db 'hbtn_0e_14_usa'
-# Sort in ascending order by cities.id
-# Display results as "<state name>: (<city id>) <city name>"
-# Script should take 3 args: username, pw, and db name
-# Must use SQLAlchemy
+"""
+14-model_city_fetch_by_state.py
+"""
 import sys
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from model_state import Base, State
 from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+
+def init_sess():
+    """initializes session and engine for sqlalchemy DB instance"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
+    return (engine, session)
 
-    for i in session.query(State, City).filter(State.id == City.state_id):
-        print("{}: ({:d}) {}".format(i.State.name, i.City.id, i.City.name))
+
+def print_cities(db):
+    """prints all the cities from session DB"""
+    session = db[1]
+    i = session.query(City, State).filter(State.id == City.state_id)
+    for c, s in i:
+        print(s.name, ': ({}) '.format(c.id), c.name, sep='')
+    session.close()
+    db[0].dispose()
+
+if __name__ == '__main__':
+    print_cities(init_sess())
