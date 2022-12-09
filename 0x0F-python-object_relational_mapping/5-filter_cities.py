ac1053a860ed23b7d9f8b900799c8eadb8cfbd2f
FateMk <93762003+FateMk@users.noreply.github.com>
2022-12-10 02:49:32 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python3
-# List all cities in state searched
-# Username, password, and database name, and state name given as user args
-# Can only use execute() once
-# Sort ascending order by cities.id
-import sys
+"""
+5-filter_cities.py
+"""
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1],
+
+def init_db():
+    """initializes a db with MySQLdb"""
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         host='localhost',
-                         port=3306)
-    cur = db.cursor()
-    cmd =  """SELECT cities.name
-         FROM states
-         INNER JOIN cities ON states.id = cities.state_id
-         WHERE states.name=%s
-         ORDER BY cities.id ASC"""
-    cur.execute(cmd, (sys.argv[4],))
-    allCities = cur.fetchall()
+                         db=sys.argv[3])
+    return db
 
-    print(", ".join([city[0] for city in allCities]))
-     
+
+def parse_input(s):
+    """parses input removing single quotes and semicolons"""
+    s = ''.join([i for i in s if i != "'" and i != ';'])
+    return s
+
+
+def print_cities_by_state(db):
+    """prints all cities from input DB"""
+    cur = db.cursor()
+    name = parse_input(sys.argv[4])
+    cur.execute("SELECT cities.name FROM cities "
+                "INNER JOIN states ON states.id = cities.state_id "
+                "WHERE states.name LIKE BINARY '%{}%' "
+                "ORDER BY cities.id ASC".format(name))
+    print(', '.join([row[0] for row in cur.fetchall()]))
     cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    print_cities_by_state(init_db())
