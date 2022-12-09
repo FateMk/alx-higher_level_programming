fa61b8d3e603bb453c522ed8d019ccceb105a7db
FateMk <93762003+FateMk@users.noreply.github.com>
2022-12-10 02:41:53 +0300

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,34 +2,31 @@
 """
 1-filter_states.py
 """
-import sys
 import MySQLdb
+import sys
 
 
 def init_db():
-    """initilizing a db with MySQLdb"""
-    db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3])
+    """initializes a db with MySQLdb"""
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
     return db
 
 
 def print_states_N(db):
-    """prints all states with a name stating with N"""
+    """prints all states with first initial 'N' from input DB"""
     cur = db.cursor()
-    cur.execute("SELECT * FROM states"
+    cur.execute("SELECT * FROM states "
                 "WHERE name LIKE BINARY 'N%' "
                 "ORDER BY states.id ASC")
-    rows = cur.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
     cur.close()
     db.close()
 
 
 if __name__ == "__main__":
-    print_states_N(int_db())
-
+    print_states_N(init_db())
