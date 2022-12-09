2e259e7862ad5a037bae0ec9e5e27ac434a7ee4a
FateMk <93762003+FateMk@users.noreply.github.com>
2022-12-10 02:56:04 +0300

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,21 +1,42 @@
 #!/usr/bin/python3
-# Print State obj with 'name' passed as arg from db 'hbtn_0e_6_usa'
-# Script should take 4 args: username, pw, db name, and state name
-# Must use SQLAlchemy
+"""
+10-model_state_my_get.py
+"""
 import sys
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from model_state import Base, State
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+
+def init_sess():
+    """initializes session instance of DB using sqlalchemy"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
+    return (engine, session)
 
-    res = session.query(State.id).filter(State.name == sys.argv[4])
 
-    if (res.first() is None):
+def parse_input(s):
+    """parses input removing single quotes and semicolons"""
+    REP_CHARS = [';', "'", '"']
+    for i in REP_CHARS:
+        s = s.replace(i, '--')
+    return s
+
+
+def print_state_name(db):
+    """prints the ID of input state"""
+    session = db[1]
+    n = parse_input(sys.argv[4])
+    r = session.query(State).filter(State.name == n)
+    try:
+        print(r[0].id)
+    except:
         print("Not found")
-    else:
-        print(res[0][0])
+    session.close()
+    db[0].dispose()
+
+
+if __name__ == '__main__':
+    print_state_name(init_sess())
