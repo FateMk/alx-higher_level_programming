fb1d298c1854d504ef51d4fff4afd5a6325ef6a7
FateMk <markmurimi151@gmail.com>
2022-12-20 14:03:53 -0800

    statu

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,11 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    res = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(res)))
-    print("\t- content: {}".format(res))
-    print("\t- utf8 content: {}".format(res.decode('utf-8')))
-
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        res = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode('utf-8')))
