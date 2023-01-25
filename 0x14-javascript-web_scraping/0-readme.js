5bafb89567256a4301b1c75d806d25c85be5be25
FateMk <93762003+FateMk@users.noreply.github.com>
2023-01-26 02:00:22 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 /* 0-redme.js */
 
-const request = require('request');
-let url = process.argv[2];
-
-request.readFile (url, 'utf8', function (error, body)  => {
- if (error) {
-  console.log(error);
- } else {
-  process.stdout.write(body);
- }
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
