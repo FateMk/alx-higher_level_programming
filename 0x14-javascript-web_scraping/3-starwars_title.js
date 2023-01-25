d736c152c2d7010ebb9fb7f9f8d0e36c1009f2d7
FateMk <93762003+FateMk@users.noreply.github.com>
2023-01-26 01:50:02 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,11 +3,14 @@
 
 const request = require('request');
 const episodeId = process.argv[2];
-const url = 'http://swapi.co/api/films/' + episodeId;
 
-request (url, function(err, res, body) {
+if (parseInt(episodeId) < 8) {
+  const url = 'http://swapi.co/api/films/' + episodeId;
+
+  request(url, (err, res, body) => {
     if (err) {
       return console.log(err);
     }
     console.log(JSON.parse(body).title);
-});
+  });
+}
