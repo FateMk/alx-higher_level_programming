b6ea86f769311b0a7f7839b89637f2bfbf50e8c5
FateMk <93762003+FateMk@users.noreply.github.com>
2023-01-26 01:51:47 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,33 +1,24 @@
-#!/usr/bin/node
-/* 4-starwars_count.js */
-
+!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-request.get(url, function (error, response, body) {
-  if (error) {
-    console.log(0);
-  }
-  if (response && body) {
-    findWedgeAntilles(response, body);
-  }
-});
 
-function findWedgeAntilles (response, body) {
-  const wedgeAntilles = '18';
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+
   let count = 0;
-  if (response.headers['content-type'] === 'application/json') {
-    body = JSON.parse(body);
-    let results = body.results;
-    for (let result in results) {
-      let characters = results[result].characters;
-      if (characters) {
-        for (let person in characters) {
-          if (characters[person].includes(wedgeAntilles)) {
-            count++;
-          }
-        }
-      }
+  let parsedBody = JSON.parse(body).results;
+
+  for (let i = 0; i < parsedBody.length; i++) {
+    let a = parsedBody[i].characters.find((c) => {
+      return c.match(/18/);
+    });
+    if (a !== undefined) {
+      count++;
     }
   }
+
   console.log(count);
-}
+});
