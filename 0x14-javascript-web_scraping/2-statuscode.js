2e64664384a1a00e2ea563c15dae7c155239ecde
FateMk <93762003+FateMk@users.noreply.github.com>
2023-01-26 02:01:27 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
 /* 2-statuscode.js */
 
-const request = require("request");
-let reqURL = process.argv[2];
-
-request (reqURL, function (error, response, body) {
-	if (error) {
-		console.log(error);
-	}
-	if (response) {
-		let st = response.statusCode;
-		console.log('code: ' + st);
-	}
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
